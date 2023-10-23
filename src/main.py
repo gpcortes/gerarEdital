9ac1cc1c71155d7801fbdaf1d7a9c46ca62ef94c
Gustavo Côrtes <desenvolvedor1@cett.org.br>
2023-10-23 10:45:28 -0300

    Change network

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from caworker import Worker
 import time
-import json
 from urllib.parse import quote
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 import pandas as pd
 from docxtpl import DocxTemplate
 from datetime import datetime, timedelta, date
-import json
 import envconfiguration as config
 import subprocess
 import re
@@ -16,6 +14,7 @@ import sys
 import locale
 from unidecode import unidecode
 from jinja2 import Environment, FileSystemLoader
+
 
 def convert_to(source, folder, timeout=None):
     args = [
@@ -94,11 +93,12 @@ def unique(list1):
 
 def get_engine(rede):
     database = ''
-    if rede  == 'efg':
+    if rede == 'efg':
         database = config.EFG_DOMAINS_DB  # type: ignore
     elif rede == 'cotec':
         database = config.COTEC_DOMAINS_DB  # type: ignore
-    string_connection = "mysql+pymysql://{user}:{password}@{host}:{port}/{database}".format(
+    string_connection = "mysql+pymysql://{user}:\
+        {password}@{host}:{port}/{database}".format(
         user=config.CAMUNDA_DOMAINS_USER,  # type: ignore
         password=quote(config.CAMUNDA_DOMAINS_PASS),  # type: ignore
         host=config.CAMUNDA_DOMAINS_HOST,  # type: ignore
@@ -108,10 +108,12 @@ def get_engine(rede):
     print(string_connection)
     return create_engine(string_connection)
 
+
 def serializar_date(obj):
     if isinstance(obj, date):
         return obj.isoformat()
     raise TypeError("Tipo de objeto não é serializável")
+
 
 def criaredital(rede):
 
@@ -154,14 +156,14 @@ def criaredital(rede):
             ee.dt_ini_insc,
             ee.dt_fim_insc,
             ee.num_edital
-        FROM Turmas_planejado_orcado tpo 
+        FROM Turmas_planejado_orcado tpo
         INNER JOIN escolas esc ON esc.id = tpo.escola_id
         LEFT JOIN udepi_municipio um ON um.escola_id = esc.id
         INNER JOIN modalidade md ON md.id = tpo.modalidade_id
         INNER JOIN tipo_curso tc ON tc.id = tpo.tipo_curso_id
         INNER JOIN cursos cr ON cr.id = tpo.curso_id
         INNER JOIN edital_ensino ee ON ee.id = tpo.num_edital_id
-        WHERE 
+        WHERE
             ee.`status`='0'
             AND ee.dt_ini_edit
             AND ee.dt_fim_edit
@@ -206,24 +208,26 @@ def criaredital(rede):
             ee.dt_ini_insc,
             ee.dt_fim_insc,
             ee.num_edital
-        FROM turmas_retificadas tret 
+        FROM turmas_retificadas tret
         INNER JOIN escolas esc ON esc.id = tret.escola_id
         LEFT JOIN udepi_municipio um ON um.escola_id = esc.id
         INNER JOIN modalidade md ON md.id = tret.modalidade_id
         INNER JOIN tipo_curso tc ON tc.id = tret.tipo_curso_id
         INNER JOIN cursos cr ON cr.id = tret.curso_id
         INNER JOIN editais_retificados ee ON ee.id = tret.num_edital_id
-        WHERE 
+        WHERE
             ee.`status`='0'
             AND ee.dt_ini_edit
             AND ee.dt_fim_edit
             AND ee.dt_ini_insc
             AND ee.dt_fim_insc is NOT null
     """, con=get_engine(rede))
-    #turmas_planejadas = turmas_planejadas[turmas_planejadas['id']==id_]
+    # turmas_planejadas = turmas_planejadas[turmas_planejadas['id']==id_]
     # turmas_planejadas.values[0]
-    #turmas_planejadas['previsao_abertura_edital'] = datetime.strftime(pd.Timestamp(turmas_planejadas['previsao_abertura_edital'].values[0]),'%Y-%m-%d')
-    #turmas_planejadas = turmas_planejadas_edital['num_edital_id'].groupby(by='num_edital_id')
+    # turmas_planejadas['previsao_abertura_edital'] = datetime.strftime(
+    #   pd.Timestamp(turmas_planejadas['previsao_abertura_edital'].values[0]),'%Y-%m-%d')
+    # turmas_planejadas = turmas_planejadas_edital['num_edital_id'].groupby(
+    #   by='num_edital_id')
 
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
@@ -285,15 +289,17 @@ def criaredital(rede):
 
     turmas_planejadas['data_resultado_final'] = datetime.strftime(
         pd.Timestamp(
-            turmas_planejadas['data_resultado_final'].values[0]),  # type: ignore
+            turmas_planejadas['data_resultado_final'].values[
+                0
+            ]),  # type: ignore
         '%d de %B de %Y') if turmas_planejadas['data_resultado_final'].values[
             0] is not None else default_date
 
     content = turmas_planejadas.to_dict('records')
 
-    result_json = turmas_planejadas.to_json(orient="index")
+    # result_json = turmas_planejadas.to_json(orient="index")
 
-    parsed_json = json.loads(result_json)
+    # parsed_json = json.loads(result_json)
 
     # content = json.loads(turmas_planejadas.to_json(orient='values'))
     # columns = [{
@@ -460,7 +466,10 @@ def criaredital(rede):
         agora = datetime.strftime(date_time_now, '%Y-%m-%d_%H-%M-%S-%f')
         ano_edital = date_time_now.year
 
-        json_string = json.dumps({'turmas_planejadas': resposta[r]}, default=serializar_date, indent=4)
+        # json_string = json.dumps(
+        #     {'turmas_planejadas': resposta[r]},
+        #     default=serializar_date,
+        #     indent=4)
         # print(json_string)
 
         BASE_DIR = '/home/python/app/outputs'
@@ -480,20 +489,27 @@ def criaredital(rede):
 
         # template_bytes = render_template(template_path, data)
 
-        # output_path = unidecode(f"{OUTPUT_PATH}/edital_{resposta[r][0]['escola']}_{resposta[r][0]['num_edital']}_{agora}.docx")
+        # output_path = unidecode(
+        #   f"{OUTPUT_PATH}/edital_{resposta[r][0]['escola']}_{resposta[r][0]['num_edital']}_{agora}.docx")
 
         # save_to_word(template_bytes, output_path)
         if resposta[r][0]['tipo_edital'] == 'retificacao':
-            doc1 = DocxTemplate("/home/python/app/templates/edital_retificacao_template.docx")
+            doc1 = DocxTemplate(
+                "/home/python/app/templates/edital_retificacao_template.docx")
         else:
-            doc1 = DocxTemplate("/home/python/app/templates/edital_template.docx")
+            doc1 = DocxTemplate(
+                "/home/python/app/templates/edital_template.docx")
         # print(doc1)
         doc1.render({'turmas_planejadas': resposta[r]})
         # print(doc1)
         if resposta[r][0]['tipo_edital'] == 'retificacao':
-            docx = unidecode(f"{OUTPUT_PATH}/edital_retificacao_{resposta[r][0]['escola']}_{resposta[r][0]['num_edital']}_{agora}.docx")
+            docx = unidecode(
+                f"{OUTPUT_PATH}/edital_retificacao_{resposta[r][0]['escola']}\
+                    _{resposta[r][0]['num_edital']}_{agora}.docx")
         else:
-            docx = unidecode(f"{OUTPUT_PATH}/edital_{resposta[r][0]['escola']}_{resposta[r][0]['num_edital']}_{agora}.docx")
+            docx = unidecode(
+                f"{OUTPUT_PATH}/edital_{resposta[r][0]['escola']}\
+                    _{resposta[r][0]['num_edital']}_{agora}.docx")
         # print(docx)
         doc1.save(docx)
 
@@ -501,17 +517,19 @@ def criaredital(rede):
         PDF_PATH = os.path.join('editais', ACS_PATH, PDF_NAME)
         print(f'Aquivo de edital gerado: "{PDF_PATH}"')
 
-        ### Rotina para atualizar o path do edital em PDF ###
+        # Rotina para atualizar o path do edital em PDF
         engine = get_engine(rede)
         connection = engine.connect()
-        table_name = 'edital_ensino' if resposta[r][0]['tipo_edital'] == 'normal' else 'editais_retificados'
+        table_name = 'edital_ensino' if \
+            resposta[r][0]['tipo_edital'] == 'normal' \
+            else 'editais_retificados'
         statement = text(f"""
-                        UPDATE {table_name}
-                        SET
-                            pdf = '{PDF_PATH}'
-                        WHERE
-                            id = {r}
-                        """)
+            UPDATE {table_name}
+            SET
+                pdf = '{PDF_PATH}'
+            WHERE
+                id = {r}
+        """)
 
         connection.execute(statement=statement)
         connection.commit()
@@ -526,7 +544,8 @@ if __name__ == '__main__':
         tasks = worker.fetch_tasks()
 
         for task in tasks:
-            rede = task.variables['nomeRede'].value if 'nomeRede' in task.variables else None
+            rede = task.variables['nomeRede'].value if \
+                'nomeRede' in task.variables else None
             criaredital(rede)
             worker.complete_task(task_id=task.id_, variables={})
             print('Inserção realizada com sucesso!')
